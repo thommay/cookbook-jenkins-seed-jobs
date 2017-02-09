63772324815c7e594cfe4dfb57078a71dead422a
Thom May <thom@chef.io>
2017-02-09 16:04:06 +0000

    only build master
    Signed-off-by: Thom May <thom@chef.io>

--- seeds.groovy
+++ seeds.groovy
@@ -3,7 +3,7 @@ import org.kohsuke.github.GitHub
 def gh = GitHub.connectAnonymously()
 gh.getOrganization('chef-cookbooks').listRepositories().each { repo ->
   job(repo.name) {
-    scm { github(repo.fullName) }
+    scm { github(repo.fullName, 'refs/heads/master') }
     steps { 
       shell('KITCHEN_LOCAL_YAML=.kitchen.docker.yml /opt/chefdk/embedded/bin/kitchen test')
     } 
