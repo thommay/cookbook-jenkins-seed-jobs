8c9be4c1dd4ef7bd2ffcb6ea8fc0c62618852763
Thom May <thom@chef.io>
2017-02-13 11:46:29 -0800

    this?
    Signed-off-by: Thom May <thom@chef.io>

--- seeds.groovy
+++ seeds.groovy
@@ -1,23 +1,25 @@
 import org.kohsuke.github.GitHub
 
 def gh = GitHub.connectAnonymously()
-gh.getOrganization('chef-cookbooks').listRepositories().each { repo ->
-  job(repo.name) {
-    scm { github(repo.fullName, 'refs/heads/master') }
-    steps { 
-      shell('KITCHEN_LOCAL_YAML=.kitchen.docker.yml /opt/chefdk/embedded/bin/kitchen test')
-    } 
-    publishers {
-      postBuildScript {
-        buildStep {
-          scriptOnlyIfFailure(false)
-          scriptOnlyIfSuccess(false)
-          shell('KITCHEN_LOCAL_YAML=.kitchen.docker.yml /opt/chefdk/embedded/bin/kitchen destroy')
+  gh.getOrganization('chef-cookbooks').listRepositories().each { repo ->
+    job(repo.name) {
+      scm { github(repo.fullName, 'refs/heads/master') }
+      steps { 
+        shell('KITCHEN_LOCAL_YAML=.kitchen.docker.yml /opt/chefdk/embedded/bin/kitchen test')
+      } 
+      publishers {
+        postBuildScript {
+          buildStep {
+            scriptOnlyIfFailure(false)
+              scriptOnlyIfSuccess(false)
+              shell {
+                command('KITCHEN_LOCAL_YAML=.kitchen.docker.yml /opt/chefdk/embedded/bin/kitchen destroy')
+              }
+          }
         }
       }
-    }
-    triggers {
+      triggers {
         cron('@daily')
+      }
     }
   }
-}
