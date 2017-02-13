ac5827f1738b29071935aeb7f1a6821b48464899
Thom May <thom@chef.io>
2017-02-13 11:35:38 -0800

    Ensure we clean up after ourselves
    Signed-off-by: Thom May <thom@chef.io>

--- seeds.groovy
+++ seeds.groovy
@@ -7,6 +7,15 @@ gh.getOrganization('chef-cookbooks').listRepositories().each { repo ->
     steps { 
       shell('KITCHEN_LOCAL_YAML=.kitchen.docker.yml /opt/chefdk/embedded/bin/kitchen test')
     } 
+    publisher {
+      postBuildScript {
+        buildStep {
+          scriptOnlyIfFailure(false)
+          scriptOnlyIfSuccess(false)
+          shell('KITCHEN_LOCAL_YAML=.kitchen.docker.yml /opt/chefdk/embedded/bin/kitchen destroy')
+        }
+      }
+    }
     triggers {
         cron('@daily')
     }
